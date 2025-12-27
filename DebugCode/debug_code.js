803b75961d934964a55ad38c25fa01e999d2babd
nisa <nisaagustiina@gmail.com>
2025-12-27 03:46:17 -0500

    debug

---
+++ DebugCode/debug_code.js
@@ -0,0 +1,46 @@
+    
+function performOperation() {
+    // Get user input from input fields
+    let num1 = parseInt(document.getElementById('input1').value);
+    let num2 = parseInt(document.getElementById('input2').value);
+    // Check if inputs are valid numbers
+    if (!isNaN(num1) && !isNaN(num2)) {
+    // Perform the operation
+        let result = multiply(num1, num2);
+
+        // Display the result
+        displayResult(result);
+    } else {
+        displayResult('Please enter valid numbers');
+    }
+}
+
+    
+function add() {
+    // Get user input from input fields
+    let num1 = parseInt(document.getElementById('input1').value);
+    let num2 = parseInt(document.getElementById('input2').value);
+    // Check if inputs are valid numbers
+    if (!isNaN(num1) && !isNaN(num2)) {
+    // Perform the operation
+        debugger;
+        let add = num1 + num2;
+
+        // Display the result
+        displayResult(add);
+    } else {
+        displayResult('Please enter valid numbers');
+    }
+}
+
+function multiply(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+    return a * b;
+}
+
+function displayResult(result) {
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `The result is: ${result}`;
+}
+        
